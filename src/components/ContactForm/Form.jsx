811c72292dd5ca06be40deb726eb6722de24bf45
Vladyslav0330 <vlad23zavadskii@gmail.com>
2023-05-10 23:14:14 +0300

    hw6

--- src/components/ContactForm/Form.jsx
+++ src/components/ContactForm/Form.jsx
@@ -1,5 +1,4 @@
-// import React, { useState } from 'react';
-// import css from '../ContactForm/Form.module.css';
+
 import { Formik, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 import { nanoid } from 'nanoid';
@@ -71,57 +70,3 @@ const FormComponent = () => {
 };
 
 export default FormComponent;
-// export function ContactForm({ onAdd }) {
-//   const [name, setName] = useState('');
-//   const [number, setNumber] = useState('');
-
-//   const handleChange = e => {
-//     const { name, value } = e.target;
-//     switch (name) {
-//       case 'name':
-//         setName(value);
-//         break;
-
-//       case 'number':
-//         setNumber(value);
-//         break;
-
-//       default:
-//         return;
-//     }
-//   };
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     onAdd(name, number);
-//     setName('');
-//     setNumber('');
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <div className={css.formBlock}>
-//         <label htmlFor="">Name</label>
-//         <input
-//           type="text"
-//           name="name"
-//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//           required
-//           onChange={handleChange}
-//           value={name}
-//         />
-//         <input
-//           type="tel"
-//           name="number"
-//           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//           required
-//           onChange={handleChange}
-//           value={number}
-//         />
-//         <button type="submit">Add contact</button>
-//       </div>
-//     </form>
-//   );
-// }
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -20,22 +20,3 @@ const Filter = () => {
 };
 
 export default Filter;
-
-// import PropTypes from 'prop-types';
-
-// export function Filter({ onChange }) {
-//   return (
-//     <label>
-//       <p>Find contacts by name</p>
-//       <input
-//         type="text"
-//         name="filter"
-//         onChange={({ target }) => onChange(target.value)}
-//       />
-//     </label>
-//   );
-// }
-
-// Filter.propTypes = {
-//   onchange: PropTypes.array,
-// };
